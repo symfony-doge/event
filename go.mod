1dac6c97f27b031353c20cdcbe1679a522eb7100
Pavel Petrov <itnelo@gmail.com>
2019-06-28 13:32:40 +0300

    read-only event listener session, go.mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/symfony-doge/event
+
+go 1.12
---
+++ ro_listener_session.go
@@ -0,0 +1,38 @@
+// Copyright 2019 Pavel Petrov <itnelo@gmail.com>. All rights reserved.
+// Use of this source code is governed by a MIT license
+// that can be found in the LICENSE file.
+
+package event
+
+// ROListenerSession is a read-only listener session that's used
+// as a bridge between event listener and sender(s) (e.g. worker); a caller
+// should pass the notification channel associated with listening session
+// to the sender(s) to start communication via events.
+type ROListenerSession struct {
+	// The base listener session implementation.
+	base *listenerSession
+
+	// A channel that should be used by senders to push their events.
+	// If a notification channel becomes closed, listening session ends.
+	// Notification channel must be closed only by Close method, it is not
+	// safe to use a built-in close function directly due to non-blocking
+	// event processing.
+	notifyChannel chan<- Event
+}
+
+// Returns a notification channel for sending events.
+func (ls ROListenerSession) NotifyChannel() chan<- Event {
+	return ls.notifyChannel
+}
+
+// Use it to safely close the notification channel and stop event listening session.
+func (ls ROListenerSession) Close() {
+	// This will ensure that all remaining events are properly processed.
+	defer ls.wait()
+
+	close(ls.notifyChannel)
+}
+
+func (ls ROListenerSession) wait() {
+	<-ls.base.done
+}
